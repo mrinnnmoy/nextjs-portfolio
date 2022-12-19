1114250a1918746cc41936fde3febe771cecb3d1
mrinnnmoy <mrinmoy202000@gmail.com>
2022-12-19 18:37:25 +0530

    update techStacks-experience-component

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -6,51 +6,75 @@ import javascript from "../public/assets/experience/javascript.png";
 import reactImage from "../public/assets/experience/react.png";
 import nextjs from "../public/assets/experience/nextjs.png";
 import tailwind from "../public/assets/experience/tailwind.png";
-import graphql from "../public/assets/experience/graphql.png";
 import node from "../public/assets/experience/node.png";
-import Image from "next/image";
+import github from "../public/assets/experience/github.png";
+import sass from "../public/assets/experience/sass.png";
+import git from "../public/assets/experience/git.png";
+import npm from "../public/assets/experience/npm.png";
+import java from "../public/assets/experience/java.png";
+import Image from 'next/image';
 
 const Experience = () => {
     const experiences = [
         {
             id: 1,
+            title: "Git",
+            src: git,
+        },
+        {
+            id: 2,
+            title: "GitHub",
+            src: github,
+        },
+        {
+            id: 3,
             title: "HTML",
             src: html,
         },
         {
-            id: 2,
+            id: 4,
             title: "CSS",
             src: css,
         },
         {
-            id: 3,
+            id: 5,
             title: "JavaScript",
             src: javascript,
         },
         {
-            id: 4,
-            title: "React",
-            src: reactImage,
-        },
-        {
-            id: 5,
-            title: "Next JS",
-            src: nextjs,
-        },
-        {
             id: 6,
-            title: "Tailwind",
-            src: tailwind,
-        },
-        {
-            id: 7,
             title: "Node JS",
             src: node,
         },
         {
+            id: 7,
+            title: "React JS",
+            src: reactImage,
+        },
+        {
             id: 8,
-            title: "GraphQL",
-            src: graphql,
+            title: "Next JS",
+            src: nextjs,
+        },
+        {
+            id: 9,
+            title: "Tailwind CSS",
+            src: tailwind,
+        },
+        {
+            id: 9,
+            title: "Sass",
+            src: sass,
+        },
+        {
+            id: 10,
+            title: "npm",
+            src: npm,
+        },
+        {
+            id: 10,
+            title: "Java",
+            src: java,
         },
     ];
 
@@ -67,13 +91,13 @@ const Experience = () => {
                     advancements in the future.
                 </p>
 
-                <div className="grid lg:grid-cols-8 gap-8">
+                <div className="grid lg:grid-cols-4 gap-8">
                     {experiences.map(({ id, title, src }) => (
                         <div
                             key={id}
                             className="flex flex-col lg:flex-row gap-10 lg:gap-0 items-center justify-between p-6 shadow-lg rounded-xl hover:scale-105 ease-in duration-300 odd:shadow-rose-400 even:shadow-blue-400">
-                            <Image src={src} alt={title} />
-                            {/* <h3 className="font-light">{title}</h3> */}
+                            <Image src={src} height={400} width={40} alt={title} />
+                            <h3 className="font-light">{title}</h3>
                         </div>
                     ))}
                 </div>
--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -14,13 +14,13 @@ const HomePage = () => {
                 </div>
 
                 <h1 className="uppercase font-bold text-gray-700 text-6xl">
-                    Full Stack Developer
+                    Frontend Developer
                 </h1>
 
                 <p className="text-gray-600 text-xl max-w-sm mx-auto">
                     Hi 👋, I am Mrinmoy Porel. I love building web application
-                    using technologies like React, Tailwind, Next JS, Node Js
-                    and GraphQL.
+                    using technologies like React JS, Next JS, Tailwind, Sass 
+                    and Node JS.
                 </p>
 
                 <div className="flex items-center justify-center gap-10">
